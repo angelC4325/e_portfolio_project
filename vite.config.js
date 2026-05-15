8be3a0491782b8aa3569a76d6202d741f0ac5359
Angel Coronel <angelcaronel@gmail.com>
2026-05-15 13:42:36 -0700

    commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/e_portfolio_project",
+  base: "/e_portfolio_project/",
 });
